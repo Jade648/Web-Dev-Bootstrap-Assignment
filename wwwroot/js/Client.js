b14686629951a054c110167b9f48b6697479d1ba
Jade 648 <maddiepeterson375@gmail.com>
2023-01-30 09:46:16 -0600

    Updated Js file

--- wwwroot/js/Client.js
+++ wwwroot/js/Client.js
@@ -14,5 +14,16 @@ $(function(){
         // play audio
         toast.play();
         $('#toast').toast({ autohide: false }).toast('show');
+
+        $('.code').on('click', function(e) {
+            e.preventDefault();
+            // first pause the audio (in case it is still playing)
+            toast.pause();
+            // reset the audio
+            toast.currentTime = 0;
+            // play audio
+            toast.play();
+            $('#toast').toast({ autohide: false }).toast('show');
     });
+});
 });
